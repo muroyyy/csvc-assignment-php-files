b62984379f242cde1e88331c019fdb57747d841c
muroyyy <amirul.faiz01@gmail.com>
2025-08-02 14:52:53 +0800

    feat(ssm): update get-parameters.php to fetch DB credentials from SSM Parameter Store
    - Switched from Secrets Manager to Systems Manager Parameter Store
    - Updated parameter name to '/msri/db-credentials'
    - Adjusted JSON keys: endpoint, db_name, username, password
    - Removed unnecessary RDS describeDBInstances call
    - Improved error handling and logging

--- get-parameters.php
+++ get-parameters.php
@@ -1,83 +1,70 @@
 <?php
-        # Retrieve settings from Parameter Store
-        error_log('Retrieving settings');
-        require 'aws-autoloader.php';
-      
-        #$az = file_get_contents('http://169.254.169.254/latest/meta-data/placement/availability-zone');
+// Load AWS SDK
+require 'aws-autoloader.php';
 
-        $ch = curl_init();
+use Aws\Ssm\SsmClient;
 
-        // get a valid TOKEN
-        $headers = array (
-                'X-aws-ec2-metadata-token-ttl-seconds: 21600' );
-        $url = "http://169.254.169.254/latest/api/token";
-        #echo "URL ==> " .  $url;
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, "PUT" );
-        curl_setopt( $ch, CURLOPT_URL, $url );
-        $token = curl_exec( $ch );
-        
-        #echo "<p> TOKEN :" . $token;
-        // then get metadata of the current instance 
-        $headers = array (
-                'X-aws-ec2-metadata-token: '.$token );
-        
-        $url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
-        
-        curl_setopt( $ch, CURLOPT_URL, $url );
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, "GET" );
-        $result = curl_exec( $ch );
-        $az = curl_exec( $ch );
-        
-        #echo "<p> RESULT :" . $result;
+error_log('Retrieving database settings from Parameter Store');
 
-        $region = substr($az, 0, -1);
-        
-        try {
-        $secrets_client = new Aws\SecretsManager\SecretsManagerClient([
-          'version' => 'latest',
-          'region'  => $region
-        ]);
-        #fetch the endpoint ep
-        $rds_client = new  Aws\Rds\RdsClient([
-          'version' => 'latest',
-          'region'  => $region
-        ]);
-        $dbresult = $rds_client->describeDBInstances();
-        $dbresult = $dbresult['DBInstances'][0]['Endpoint']['Address'];
-        $ep = $dbresult;
-        #echo $ep;
-        #
-        #fetch secrets for the endpoint
-        $secretresults = $secrets_client->listSecrets(array(
-          ['Key'=>['name'],
-          'Values'=>['rds!']
-          ])
-          );
-          $result = $secrets_client->getSecretValue([
-            'SecretId' => $secretresults['SecretList'][0]['Name'],
-        ]);
-        $result = $result['SecretString'];
-        $result = json_decode($result, true);
-  #      echo $result;
+// Step 1: Get the instance region
+$ch = curl_init();
 
-        #$result = $result['SecretString'];
-   #     print($result);
-        #$result = json_decode($result, true);
-        $un = $result['username'];
-        $pw = $result['password'];
-        $db = 'countries';
+// Get a valid TOKEN
+$headers = array(
+    'X-aws-ec2-metadata-token-ttl-seconds: 21600'
+);
+$url = "http://169.254.169.254/latest/api/token";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+curl_setopt($ch, CURLOPT_URL, $url);
+$token = curl_exec($ch);
 
-        }
-        catch (Exception $e) {
-          $ep = '';
-          $db = '';
-          $un = '';
-          $pw = '';
-        }
-      error_log('Settings are: ' . $ep. " / " . $db . " / " . $un . " / " . $pw);
-      #echo " Check your Database settings ";
-      ?>
+// Get availability zone
+$headers = array(
+    'X-aws-ec2-metadata-token: '.$token
+);
+$url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+$az = curl_exec($ch);
+curl_close($ch);
+
+// Convert AZ to region (remove last letter)
+$region = substr($az, 0, -1);
+
+try {
+    // Step 2: Create SSM client
+    $ssmClient = new SsmClient([
+        'version' => 'latest',
+        'region'  => $region
+    ]);
+
+    // Step 3: Get parameter value from Parameter Store
+    $param = $ssmClient->getParameter([
+        'Name' => '/msri/db-credentials',
+        'WithDecryption' => true
+    ]);
+
+    // Step 4: Decode the JSON value
+    $result = json_decode($param['Parameter']['Value'], true);
+
+    // Assign variables
+    $ep = $result['endpoint'];
+    $db = $result['db_name'];
+    $un = $result['username'];
+    $pw = $result['password'];
+
+} catch (Exception $e) {
+    $ep = '';
+    $db = '';
+    $un = '';
+    $pw = '';
+    error_log('Error retrieving database settings: ' . $e->getMessage());
+}
+
+error_log('Settings are: ' . $ep . " / " . $db . " / " . $un . " / " . $pw);
+// echo "Check your Database settings";
+?>
